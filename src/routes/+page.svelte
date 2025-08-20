3f130d6962993e559d5c24e286316c210e053c82
micaelaalvaradomendez <micaelaalvaradomendez@gmail.com>
2025-08-20 12:11:04 -0300

    page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<h1>Sistemas operativos - UNTDF - 2025</h1>
+<h2>Getting started</h2>
+<p>Micaela Alvarado Mendez</p>
+
